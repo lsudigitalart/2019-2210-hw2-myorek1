58984e115f6e9455934efe5e1f5725884e1aff33
myorek1 <myorek1@lsu.edu>
2019-09-10 13:01:26 -0500

    4

--- sketch.js
+++ sketch.js
@@ -15,14 +15,58 @@ function setup() {
 
     // creating a loop for the dots in the background 
     // for (init; test; update){statments} 
-    for(var i = 0; i<400; i+=2){
-      point(i,i+2);
+
+    /*
+    for (var t = 0; t <500; t+=2){
+      
+    for(var i = 0; i<500; i+=2){
+      point(i,i+t);
     }
+    for(var i = 0; i<500; i+=2){
+      point(i+2,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i+4,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i-2,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i-4,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i-6,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i-8,i+2);
+    }
+    for(var i = 0; i<500; i+=2){
+      point(i+6,t+2);
+    }
+  }
+  */
+  // hairline 
+// remember canvas size is 550,330 where length is 550 and heaight is 330
+  
+point(440,329)
+point(460,300)
+point(480,250)
+point(475,250)
+point(438,300)
+point(415,329)
 
-    //point(240,60);
-    //point(230,60);
-
-   
-
+// line by eye
+fill(5);
+stroke(2);
+beginShape();
+curveVertex(440,330)
+curveVertex(440,339);
+curveVertex(460,300);
+curveVertex(480,250);
+curveVertex(475,250);
+curveVertex(438,300);
+curveVertex(415,330);
+curveVertex(415,330)
+endShape();
 
   }
