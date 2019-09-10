b1fd1dd43cb59e5e888ac5ca3ca85e5f985c73d7
myorek1 <myorek1@lsu.edu>
2019-09-09 20:35:47 -0500

    Update first dot loop

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-    // Put things you want to run once in here. 
+    // Put things you want to run once in here.
+    
+    createCanvas(500,330);
+    
+  
   }
   
   function draw() {
     // Put things you want to over and over here. 
+    // tan background color
+    background(239,220,210);
+
+    // dots in the background
+
+    // creating a loop for the dots in the background 
+    // for (init; test; update){statments} 
+    for(var i = 0; i<400; i+=2){
+      point(i,i+2);
+    }
+
+    //point(240,60);
+    //point(230,60);
+
+   
+
+
   }
