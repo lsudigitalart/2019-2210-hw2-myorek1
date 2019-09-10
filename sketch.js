721178641213fc8f2e6c86ea2a68b5fb9717636e
myorek1 <myorek1@lsu.edu>
2019-09-10 13:25:18 -0500

    hair part 1

--- sketch.js
+++ sketch.js
@@ -48,12 +48,33 @@ function setup() {
   // hairline 
 // remember canvas size is 550,330 where length is 550 and heaight is 330
   
-point(440,329)
-point(460,300)
-point(480,250)
-point(475,250)
-point(438,300)
-point(415,329)
+fill(5)
+stroke(2)
+beginShape();
+curveVertex(0,280);
+curveVertex(1,280);
+curveVertex(12,272);
+curveVertex(3,272);
+curveVertex(15,260);
+curveVertex(4,255);
+curveVertex(35,240);
+curveVertex(45,200);
+curveVertex(65,170);
+curveVertex(85,160);
+curveVertex(120,135);
+curveVertex(110,132);
+curveVertex(122,115);
+curveVertex(110,116);
+curveVertex(135,95);
+curveVertex(140,70);
+curveVertex(130,70);
+curveVertex(142,60);
+curveVertex(120,50);
+curveVertex(114,50);//use this as next start point for hairline
+curveVertex(0,40);
+curveVertex(0,40);
+
+endShape();
 
 // line by eye
 fill(5);
